09bbc3a5d095ee0b45b863274d8efed38a835eef
Stefan Bjelcevic <stbjelcevic@gmail.com>
2018-04-13 11:23:21 -0400

    backend fix

--- Backend/app.py
+++ Backend/app.py
@@ -2,23 +2,10 @@ import os
 from flask import Flask, render_template, request, json
 from sklearn.externals import joblib
 
-
-# set the template directory to the Frontend folder
-template_dir = os.path.abspath('../Frontend/')
-# not sure if next line is necessary...
-# static_dir = os.path.abspath('static/')
-app = Flask(__name__, template_folder=template_dir)  #, static_folder=static_dir)
+app = Flask(__name__)
 
 
-@app.route("/ourURL")
-def main():
-    # url_for('static', filename='popper.min.js')
-    # url_for('static', filename='awesomplete.js')
-    # url_for('static', filename='awesomplete.css')
-    return render_template('index.html')
-
-
-@app.route('/ourURL/ols', methods=['GET'])
+@app.route('/ols', methods=['GET'])
 def getLinRegPrediction():
     rating = request.form['rating']
     runtime = request.form['runtime']
@@ -41,7 +28,7 @@ def getLinRegPrediction():
                        'plot': plot})
 
 
-@app.route('/ourURL/svm', methods=['GET'])
+@app.route('/svm', methods=['GET'])
 def getSVMPrediction():
     rating = request.form['rating']
     runtime = request.form['runtime']
